1a7bc2c5ca69b9a5fdd4373431a8004eba0a083e
Rafael Santos <rafaelgamail9@gmail.com>
2024-05-22 17:37:10 -0300

    Commit arquivo exercicio_41.js

---
+++ exercicio_41.js
@@ -0,0 +1,15 @@
+// Objeto pessoa com propriedades nome e idade
+let pessoa = {
+    nome: "Rafael",
+    idade: 21
+  };
+  
+  // Acessar e imprimir o valor da propriedade idade
+  console.log("Idade da pessoa:", pessoa.idade);
+  
+  // Adicionar uma nova propriedade chamada email ao objeto pessoa
+  pessoa.email = "rafaelsantos@gmail.com";
+  
+  // Imprimir o objeto pessoa atualizado
+  console.log("Pessoa atualizada:", pessoa);
+  
